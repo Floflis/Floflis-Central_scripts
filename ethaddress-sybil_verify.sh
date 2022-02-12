86a624e4252a2bea18ab944099f9e019406b5b2d
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-12 15:00:35 -0300

    ethaddress-sybil_verify: store Sybil.org's Twitter handle at /1/config/user.json

--- ethaddress-sybil_verify.sh
+++ ethaddress-sybil_verify.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ "$(jq -r '.sybil' /1/config/user.json)" = "null" ]; then
+if [ "$(jq -r '.sybiltwitter' /1/config/user.json)" = "null" ]; then
    echo "Its your first time using 'ethaddress-sybil_verify'!"
    echo "Initializing..."
-   cat /1/config/user.json | jq '. + {"sybil": ""}' | tee /1/config/user.json
+   cat /1/config/user.json | jq '. + {"sybiltwitter": ""}' | tee /1/config/user.json
 fi
 
 letsgo () {
@@ -12,9 +12,13 @@ ethaddress="$(jq -r '.eth' /1/config/user.json)"
 twitterprofile="$(curl -s "https://raw.githubusercontent.com/Uniswap/sybil-list/master/verified.json" | jq -r ".\"$ethaddress\".twitter.handle")"
 
 if [ "$twitterprofile" = "null" ]; then
+   contents="$(jq '.sybiltwitter = "no"' /1/config/user.json)" && \
+   echo "${contents}" > /1/config/user.json
    echo "Account $ethaddress have no verified Twitter profile at Sybil.org"
    
 else
+   contents="$(jq ".sybiltwitter = \"$twitterprofile\"" /1/config/user.json)" && \
+   echo "${contents}" > /1/config/user.json
    echo "$ethaddress's verified Twitter profile at Sybil.org is:"
    echo "$twitterprofile"
 fi
