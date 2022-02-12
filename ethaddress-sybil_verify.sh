aad1139ab800392980f4457d74492ea062c79e30
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-12 14:30:51 -0300

    ethaddress-sybil_verify: detect if first time using, then initialize with .sybil field at user.json

--- ethaddress-sybil_verify.sh
+++ ethaddress-sybil_verify.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ "$(jq -r '.sybil' /1/config/user.json)" = "null" ]; then
+   echo "Its your first time using 'ethaddress-sybil_verify'!"
+   echo "Initializing..."
+   cat /1/config/user.json | jq '. + {"sybil": ""}' | tee /1/config/user.json
+fi
+
 letsgo () {
 ethaddress="$(jq -r '.eth' /1/config/user.json)"
 
@@ -7,6 +13,10 @@ twitterprofile="$(curl -s "https://raw.githubusercontent.com/Uniswap/sybil-list/
 
 if [ "$twitterprofile" = "null" ]; then
    echo "Account $ethaddress have no verified Twitter profile at Sybil.org"
+   
+else
+   echo "$ethaddress's verified Twitter profile at Sybil.org is:"
+   echo "$twitterprofile"
 fi
 }
 
