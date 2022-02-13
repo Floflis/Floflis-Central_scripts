808c181929672317c063c429aba9adcb7d37cd34
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-12 22:02:48 -0300

    improve fullname.sh
    Only create JSON field when initializing

--- fullname.sh
+++ fullname.sh
@@ -6,7 +6,11 @@ user_gecos_field="$(echo "$user_record" | cut -d ':' -f 5)"
 user_full_name="$(echo "$user_gecos_field" | cut -d ',' -f 1)"
 echo "$user_full_name"
 
-tmp="$(mktemp)"; cat /1/config/user.json | jq '. + {fullname: ""}' >"$tmp" && mv "$tmp" /1/config/user.json
+if [ "$(jq -r '.fullname' /1/config/user.json)" = "null" ]; then
+   echo "Its your first time using fullname.sh!"
+   echo "Initializing..."
+   tmp="$(mktemp)"; cat /1/config/user.json | jq '. + {fullname: ""}' >"$tmp" && mv "$tmp" /1/config/user.json
+fi
 
 contents="$(jq ".fullname = \"$user_full_name\"" /1/config/user.json)" && \
 echo "${contents}" > /1/config/user.json
