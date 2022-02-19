d9a9948a658f01376feab56513a8a1a0a4d0cbec
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-19 20:31:52 -0300

    ensname.sh and fullname.sh: detailed status updating/logging

--- ensname.sh
+++ ensname.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+echo "Getting user's ENS name..."
+
 ethaddress="$(jq -r '.eth' /1/config/user.json)"
 ensname="$(ethereal ens domain get --address=$ethaddress)"
+
+echo "It is: $ethaddress"
 
 if [ "$(jq -r '.ens' /1/config/user.json)" = "null" ]; then
    echo "Its your first time using ensname.sh!"
@@ -11,3 +15,9 @@ fi
 
 contents="$(jq ".ens = \"$ensname\"" /1/config/user.json)" && \
 echo "${contents}" > /1/config/user.json
+
+if [ "$(jq -r '.ens' /1/config/user.json)" != "null" ]; then
+   if [ "$(jq -r '.ens' /1/config/user.json)" = "$ensname" ]; then
+      echo "Done!"
+fi
+fi
--- fullname.sh
+++ fullname.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+echo "Getting user's full name..."
+
 user_name="$(logname)"
 user_record="$(getent passwd $user_name)"
 user_gecos_field="$(echo "$user_record" | cut -d ':' -f 5)"
 user_full_name="$(echo "$user_gecos_field" | cut -d ',' -f 1)"
-echo "$user_full_name"
+#echo "$user_full_name"
+# https://stackoverflow.com/a/833256/5623661 (sorry for crediting the answer from someone who say "hysterical")
+
+echo "It is: $user_full_name"
 
 if [ "$(jq -r '.fullname' /1/config/user.json)" = "null" ]; then
    echo "Its your first time using fullname.sh!"
@@ -14,4 +19,9 @@ fi
 
 contents="$(jq ".fullname = \"$user_full_name\"" /1/config/user.json)" && \
 echo "${contents}" > /1/config/user.json
-# https://stackoverflow.com/a/833256/5623661 (sorry for crediting the answer from someone who say "hysterical")
+
+if [ "$(jq -r '.fullname' /1/config/user.json)" != "null" ]; then
+   if [ "$(jq -r '.fullname' /1/config/user.json)" = "$user_full_name" ]; then
+      echo "Done!"
+fi
+fi
