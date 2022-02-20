f41a55fd3107515bcedf3e120a19e539fae95665
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-20 17:03:29 -0300

    nfts-parse.sh: add Axie and Rockstars of EPNS' balances

--- nfts-parse.sh
+++ nfts-parse.sh
@@ -20,7 +20,7 @@ if [ "$(jq -r '.danimesqart' /1/config/nfts.json)" = "null" ] || [ "$(jq -r '.da
 ENDOFFILE
 fi
 
-if [ "$(jq -r '.x' /1/config/nfts.json)" = "null" ]; then
+if [ "$(jq -r '.expansionpunks' /1/config/nfts.json)" = "null" ]; then
    echo "Initializing for ExpansionPunks NFT..."
    #tmp="$(mktemp)"; cat /1/config/nfts.json | jq '. + "x":{"1":[{"balance":"","metadata":"","name":"","description":"","image":""}]}' >"$tmp" && mv "$tmp" /1/config/nfts.json
    #tmp0="$(mktemp)"; cat /1/config/nfts.json | jq '. + "x":{"1":[{"balance":"","metadata":"","name":"","description":"","image":""}]}' >"$tmp" && mv "$tmp" /1/config/nfts.json
@@ -30,13 +30,22 @@ if [ "$(jq -r '.x' /1/config/nfts.json)" = "null" ]; then
    tmp="$(mktemp)"; cat /1/config/nfts.json | jq '. + {"expansionpunks":{"1":[{"balance":"","metadata":"","name":"","description":"","image":""}]}}' >"$tmp" && mv "$tmp" /1/config/nfts.json
 fi
 
+if [ "$(jq -r '.axiepet' /1/config/nfts.json)" = "null" ]; then
+   echo "Initializing for Axie pet NFT..."
+   tmp="$(mktemp)"; cat /1/config/nfts.json | jq '. + {"axiepet":{"1":[{"balance":"","metadata":"","name":"","description":"","image":""}]}}' >"$tmp" && mv "$tmp" /1/config/nfts.json
+fi
+
+if [ "$(jq -r '.rockstarsofepns' /1/config/nfts.json)" = "null" ]; then
+   echo "Initializing for Rockstars of EPNS NFT..."
+   tmp="$(mktemp)"; cat /1/config/nfts.json | jq '. + {"rockstarsofepns":{"1":[{"balance":"","metadata":"","name":"","description":"","image":""}]}}' >"$tmp" && mv "$tmp" /1/config/nfts.json
+fi
+
 processbalances () {
 if [ "$currentnft" = "danimesqart" ]; then
    currentnft_ethereum="0x1b6b3026B7F5531086f3B8e6CC31C6564BBE2849"
    currentnft_polygon=""
    currentnft_xdai=""
    currentnft_abi="universal-abi.json"
-#   currentnft_coingeckoname="ethereum"
    echo "Updating Daniell Mesquita's Arts NFT..."
 fi
 
@@ -45,8 +54,23 @@ if [ "$currentnft" = "expansionpunks" ]; then
    currentnft_polygon=""
    currentnft_xdai=""
    currentnft_abi="universal-abi.json"
-#   currentnft_coingeckoname="ethereum"
    echo "Updating ExpansionPunks NFT..."
+fi
+
+if [ "$currentnft" = "axiepet" ]; then
+   currentnft_ethereum="0xf5b0a3efb8e8e4c201e2a935f110eaaf3ffecb8d"
+   currentnft_polygon=""
+   currentnft_xdai=""
+   currentnft_abi="abi-axie.json"
+   echo "Updating Axie pet NFT..."
+fi
+
+if [ "$currentnft" = "rockstarsofepns" ]; then
+   currentnft_ethereum="0x3f8C2152b79276b78315CAF66cCF951780580A8a"
+   currentnft_polygon=""
+   currentnft_xdai=""
+   currentnft_abi="abi-rockstars_of_epns.json"
+   echo "Updating Rockstars of EPNS NFT..."
 fi
 
 if [ "$currentnft_ethereum" != "" ]; then
@@ -85,4 +109,14 @@ echo "${contents}" > /1/config/nfts.json
 currentnft="expansionpunks"
 processbalances
 contents="$(jq ".expansionpunks.\"1\"[].balance = \"$nftbalanceeth\"" /1/config/nfts.json)" && \
+echo "${contents}" > /1/config/nfts.json
+
+currentnft="axiepet"
+processbalances
+contents="$(jq ".axiepet.\"1\"[].balance = \"$nftbalanceeth\"" /1/config/nfts.json)" && \
+echo "${contents}" > /1/config/nfts.json
+
+currentnft="rockstarsofepns"
+processbalances
+contents="$(jq ".rockstarsofepns.\"1\"[].balance = \"$nftbalanceeth\"" /1/config/nfts.json)" && \
 echo "${contents}" > /1/config/nfts.json
