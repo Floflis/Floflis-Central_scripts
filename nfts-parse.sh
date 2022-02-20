c72bf121685f9f6180e653fd02f13ba041063f84
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-20 15:51:13 -0300

    nfts-parse.sh: start getting balances of its first NFT

--- nfts-parse.sh
+++ nfts-parse.sh
@@ -9,6 +9,7 @@ if [ "$(jq -r '.danimesqart' /1/config/nfts.json)" = "null" ] || [ "$(jq -r '.da
 {
 	"danimesqart": {
 		"1": [{
+		        "balance": "",
 			"metadata": "",
 			"name": "",
 			"description": "",
@@ -24,20 +25,31 @@ if [ "$currentnft" = "danimesqart" ]; then
    currentnft_ethereum="0x1b6b3026B7F5531086f3B8e6CC31C6564BBE2849"
    currentnft_polygon=""
    currentnft_xdai=""
-   currentnft_abi="abis/universal-abi.json"
+   currentnft_abi="universal-abi.json"
 #   currentnft_coingeckoname="ethereum"
    echo "Updating Daniell Mesquita's Arts NFT..."
 fi
 
 if [ "$currentnft_ethereum" != "" ]; then
-   #ethereal contract call --contract=$currentnft_ethereum --abi=$currentnft_abi --call="balanceOf(\"$ethaddress\")" --from=$ethaddress
-   cd abis
-   test="$(ethereal contract call --contract=$currentnft_ethereum --abi=universal-abi.json --call="balanceOf($ethaddress)" --from=$ethaddress)"
-   echo "$test"
-#   --abi=$currentnft_abi
+   nftFULLbalanceeth="$(ethereal contract call --contract=$currentnft_ethereum --abi=abis/$currentnft_abi --call="balanceOf($ethaddress)" --from=$ethaddress)"
+   echo "Balance: $nftFULLbalanceeth"
+#   nftbalanceeth="$(printf "%.2f\n" $(echo "$nftFULLbalanceeth" | bc -l))"
 else
-   echo ""
+   if [ "$currentnft_polygon" != "" ]; then
+      nftFULLbalancematic="$(ethereal --connection=$rpc_polygon contract call --contract=$currentnft_ethereum --abi=abis/$currentnft_abi --call="balanceOf($ethaddress)" --from=$ethaddress)"
+#      nftbalancematic="$(printf "%.2f\n" $(echo "$nftFULLbalancematic" | bc -l))"
 fi
+
+   if [ "$currentnft_xdai" != "" ]; then
+      nftFULLbalancexdai="$(ethereal --connection=$rpc_xdai contract call --contract=$currentnft_ethereum --abi=abis/$currentnft_abi --call="balanceOf($ethaddress)" --from=$ethaddress)"
+#      nftbalancexdai="$(printf "%.2f\n" $(echo "$nftFULLbalancexdai" | bc -l))"
+fi
+fi
+
+#nfttotalbalance=$(echo "$tokenbalanceeth + $tokenbalancematic + $tokenbalancexdai"|bc)
+
+#nfttotalbalance=$(echo "$nftFULLbalanceeth + $nftFULLbalancematic + $nftFULLbalancexdai"|bc)
+#echo "total bal: $nfttotalbalance"
 }
 
 currentnft="danimesqart"
