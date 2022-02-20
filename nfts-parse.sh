c17a9e203c1ed96aa9f6be9428f6070c5143f3af
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-20 15:32:39 -0300

    Updates to nfts-parse.sh

--- nfts-parse.sh
+++ nfts-parse.sh
@@ -2,7 +2,7 @@
 
 ethaddress="$(jq -r '.eth' /1/config/user.json)"
 
-if [ "$(jq -r '.flof' /1/config/nfts.json)" = "null" ] || [ "$(jq -r '.flof' /1/config/nfts.json)" = "" ]; then
+if [ "$(jq -r '.danimesqart' /1/config/nfts.json)" = "null" ] || [ "$(jq -r '.danimesqart' /1/config/nfts.json)" = "" ]; then
    echo "Its your first time using nfts-parse.sh!"
    echo "Initializing..."
    cat > /1/config/nfts.json << ENDOFFILE
@@ -24,15 +24,19 @@ if [ "$currentnft" = "danimesqart" ]; then
    currentnft_ethereum="0x1b6b3026B7F5531086f3B8e6CC31C6564BBE2849"
    currentnft_polygon=""
    currentnft_xdai=""
-   currentnft_abi="universal-abi.json"
+   currentnft_abi="abis/universal-abi.json"
 #   currentnft_coingeckoname="ethereum"
    echo "Updating Daniell Mesquita's Arts NFT..."
 fi
 
 if [ "$currentnft_ethereum" != "" ]; then
-   ethereal contract call --contract=$currentnft_ethereum --abi=abis/$currentnft_abi --call="balanceOf(\"$ethaddress\")" --from=$ethaddress
+   #ethereal contract call --contract=$currentnft_ethereum --abi=$currentnft_abi --call="balanceOf(\"$ethaddress\")" --from=$ethaddress
+   cd abis
+   test="$(ethereal contract call --contract=$currentnft_ethereum --abi=universal-abi.json --call="balanceOf($ethaddress)" --from=$ethaddress)"
+   echo "$test"
+#   --abi=$currentnft_abi
 else
-   
+   echo ""
 fi
 }
 
